5281cc79e9d1539dcdfc4582a6fc9c54f962823f
Hunter Trawick <trawick84@gmail.com>
2021-01-30 19:53:38 -0700

    Updated styles

--- components/banner.js
+++ components/banner.js
@@ -35,76 +35,74 @@ const Banner = (props) => {
 
   return (
     <div className="main-bg">
-      <div className="greyscale py-5">
-        <Container fluid="lg">
-          <div className="mx-md-5 px-md-5">
-            <Row className="search-section px-sm-3">
-              <Col className="search-height mb-2" sm={6}>
-                <p className="search-heading">Check in &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; / &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Check out</p>
-                <DateRangePicker
-                  startDateId="startDate"
-                  endDateId="endDate"
-                  startDate={startDate}
-                  endDate={endDate}
-                  onDatesChange={({ startDate, endDate }) => {
-                    setStartDate(startDate);
-                    setEndDate(endDate);
-                  }}
-                  focusedInput={focusedInput}
-                  onFocusChange={(focusedInput) => {
-                    setFocusedInput(focusedInput);
-                  }}
-                  noBorder={true}
-                  customArrowIcon={<div/>}
-                  orientation={windowWidth <= 575 ? "vertical" : "horizontal"}
-                  daySize={windowWidth <= 575 ? 30:40}
-                />
-              </Col>
+      <div className="greyscale">
+        <Container className="col-12 col-lg-10 offset-lg-1 py-5">
+          <Row className="search-section px-sm-3">
+            <Col className="search-height mb-2" sm={6}>
+              <p className="search-heading">Check in &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; / &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Check out</p>
+              <DateRangePicker
+                startDateId="startDate"
+                endDateId="endDate"
+                startDate={startDate}
+                endDate={endDate}
+                onDatesChange={({ startDate, endDate }) => {
+                  setStartDate(startDate);
+                  setEndDate(endDate);
+                }}
+                focusedInput={focusedInput}
+                onFocusChange={(focusedInput) => {
+                  setFocusedInput(focusedInput);
+                }}
+                noBorder={true}
+                customArrowIcon={<div/>}
+                orientation={windowWidth <= 575 ? "vertical" : "horizontal"}
+                daySize={windowWidth <= 575 ? 30:40}
+              />
+            </Col>
 
-              <Col className="search-height" xs={12} sm={3}>
-                <p className="search-heading">Guests</p>
-                <Form.Group
-                  controlId="propertySearchGuestCount"
-                  className="mb-0 select-guest"
+            <Col className="search-height" xs={12} sm={3}>
+              <p className="search-heading">Guests</p>
+              <Form.Group
+                controlId="propertySearchGuestCount"
+                className="mb-0 select-guest"
+              >
+                <Form.Control
+                  as="select"
+                  placeholder="Select guests"
+                  onChange={(evt) => {
+                    setGuests(evt.target.value);
+                  }}
+                  size="sm"
+                  value={guests}
                 >
-                  <Form.Control
-                    as="select"
-                    placeholder="Select guests"
-                    onChange={(evt) => {
-                      setGuests(evt.target.value);
-                    }}
-                    size="sm"
-                    value={guests}
-                  >
-                    <option value={1}>1 guest</option>
-                    <option value={2}>2 guests</option>
-                    <option value={3}>3 guests</option>
-                    <option value={4}>4 guests</option>
-                    <option value={5}>5 guests</option>
-                    <option value={6}>6 guests</option>
-                    <option value={7}>7 guests</option>
-                    <option value={8}>8 guests</option>
-                    <option value={9}>9 guests</option>
-                    <option value={10}>10 guests</option>
-                    <option value={11}>11 guests</option>
-                    <option value={12}>12+ guests</option>
-                  </Form.Control>
-                </Form.Group>
-              </Col>
+                  <option value={1}>1 guest</option>
+                  <option value={2}>2 guests</option>
+                  <option value={3}>3 guests</option>
+                  <option value={4}>4 guests</option>
+                  <option value={5}>5 guests</option>
+                  <option value={6}>6 guests</option>
+                  <option value={7}>7 guests</option>
+                  <option value={8}>8 guests</option>
+                  <option value={9}>9 guests</option>
+                  <option value={10}>10 guests</option>
+                  <option value={11}>11 guests</option>
+                  <option value={12}>12+ guests</option>
+                </Form.Control>
+              </Form.Group>
+            </Col>
 
-              <Col className="search-height pr-0 text-center text-md-right" xs={12} sm={3}>
-                <Link href={getSearchLink(startDate, endDate, guests)}>
-                  <Button variant="primary" className="book-btn">
-                    Book now
-                  </Button>
-                </Link>
-              </Col>
-            </Row>
-          </div>
-          <Row className="mt-5 pt-2">
+            <Col className="search-height pr-0 text-center text-md-right" xs={12} sm={3}>
+              <Link href={getSearchLink(startDate, endDate, guests)}>
+                <Button variant="primary" className="book-btn">
+                  Book now
+                </Button>
+              </Link>
+            </Col>
+          </Row>
+          <Row className="info-carousel-section align-items-center">
             <Col xs={12} md={6} lg={6} sm={6}>
               <h1>ZION VILLAGE RESORT</h1>
-              <p className="banner-text">
+              <p className="banner-text font-weight-bold">
                 With the perfect balance of relaxation and adventure, Zion
                 Village is the perfect getaway for anyone looking to enjoy all
                 Southern Utah has to offer. Offering brand new townhomes and
@@ -117,7 +115,7 @@ const Banner = (props) => {
                 <i className="fal fa-arrow-right arrow-icon"></i>
               </Button>
             </Col>
-            <Col xs={10} sm={5} md={5} lg={5} style={{ alignSelf: "center" }}>
+            <Col xs={10} sm={5} md={5} lg={4} style={{ alignSelf: "center" }} className="offset-lg-1 pr-0">
               <Slider {...settings} ref={sliderRef}>
                 {Object.keys(props.properties).map((item, index) => {
                    return (
@@ -134,7 +132,7 @@ const Banner = (props) => {
                 )})}
               </Slider>
             </Col>
-            <Col xs={2} sm={1} md={1} lg={1} style={{ alignSelf: "center" }}>
+            <Col xs={2} sm={1} md={1} lg={1} className="pr-0" style={{ alignSelf: "center" }}>
               {Object.keys(props.properties).map((item, index) => (
                 props.properties[item] && props.properties[item].published &&
                   <div
